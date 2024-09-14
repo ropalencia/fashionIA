84150fcc4e33e89cef79343268a6f7b725fcbb2e
ropalencia <81266375+ropalencia@users.noreply.github.com>
2024-09-14 09:44:12 -0500

    ajustes para mejorar rutas

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ model.add(GlobalAveragePooling2D())
 
 
 # Definir rutas de archivos
-train_features_path = 'Trabajo 3/Modelos/train_features.pkl'
-val_features_path = 'Trabajo 3/Modelos/val_features.pkl'
-pca_model_path = 'Trabajo 3/Modelos/pca_model.pkl'
-knn_model_path = 'Trabajo 3/Modelos/knn_model.pkl'
-val_path = 'Trabajo 3/Modelos/val_data.pkl'
+train_features_path = './Modelos/train_features.pkl'
+val_features_path = './Modelos/val_features.pkl'
+pca_model_path = './Modelos/pca_model.pkl'
+knn_model_path = './Modelos/knn_model.pkl'
+val_path = './Modelos/val_data.pkl'
 
 
 # Cargar el PCA y KNN desde los archivos pickle
--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,25 @@
-import pickle
-train_features_path = 'Trabajo 3/Modelos/train_features.pkl'
-val_features_path = 'Trabajo 3/Modelos/val_features.pkl'
-pca_model_path = 'Trabajo 3/Modelos/pca_model.pkl'
-knn_model_path = 'Trabajo 3/Modelos/knn_model.pkl'
-val_path = 'Trabajo 3/Modelos/val_data.pkl'
+from PIL import Image
 
+# Define la ruta de la imagen
+image_path = "./Datos/extracted_files/images\\42872.jpg"
+
+#Datos\extracted_files\images\1164.jpg
+#C:\Users\DELL\Documents\GitHub\fashionIA\Trabajo 3\Datos\extracted_files\images\1164.jpg
+# Abre la imagen
+img = Image.open(image_path)
+import pickle
+# Muestra la imagen
+#img.show()
+
+
+
+
+# Definir rutas de archivos
+train_features_path = './Modelos/train_features.pkl'
+val_features_path = './Modelos/val_features.pkl'
+pca_model_path = './Modelos/pca_model.pkl'
+knn_model_path = './Modelos/knn_model.pkl'
+val_path = './Modelos/val_data.pkl'
 
 # Cargar el PCA y KNN desde los archivos pickle
 with open(pca_model_path, 'rb') as f:
